ffdbf3d3ec6dbd081862c6cf01ff2e38c8f11d61
Tiffany Yu <tiffanyayu@gmail.com>
2016-05-17 14:48:55 +0200

    Added book images

--- src/app.js
+++ src/app.js
@@ -9,3 +9,16 @@ app.get ( '/', ( req, res) => {
 } )
 
 app.listen ( 3000 )
+
+// BOOK APP PLAN
+// // Make an index
+// // Have a book database
+// // // Boks need title, author, description, thumbnail, cover
+// Resources folder
+// Images folder
+// Get book images
+// Tell expess to use a static folder
+// Make script to control DOM and do AJAX
+// DOM (result of rendering the page): listen for clicks
+// AJAX: Get book details
+// Create book API (place to request data and come back with the data)
