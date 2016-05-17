a2f9e99e77e560cbc832c69c643e4792d4191356
Tiffany Yu <tiffanyayu@gmail.com>
2016-05-17 17:21:32 +0200

    Updated process list

--- src/app.js
+++ src/app.js
@@ -48,13 +48,13 @@ app.listen ( 3000 )
 // BOOK APP PLAN
 // // X Make an index
 // // X Have a book database
-// // // X Boks need title, author, description, thumbnail, cover
+// // // X Books need title, author, description, thumbnail, cover
 // X Resources folder
 // X Images folder
 // X Get book images
 // X Tell express to use a static folder
-// X Show the books on thge page
+// X Show the books on the page
 // Make script to control DOM and do AJAX
 // DOM (result of rendering the page): listen for clicks
-// AJAX: Get book details
 // Create book API (place to request data and come back with the data)
+// AJAX: Get book details
